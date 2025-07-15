e83584d76e895bdc3ecc3025d14d4d21bdd03a0e
yadhim <yhim059@gmail.com>
2025-07-15 16:32:43 +0300

    this is my code

--- fırst.cpp
+++ fırst.cpp
@@ -1,55 +1,57 @@
+cpp
 #include <iostream>
-#include <map>
-#include <string>
 using namespace std;
 
-int main(){
- map<string, int> inventory;
 
- while (true) {
-    cout << "\nOptions:\n";
-    cout << "1. add fruit\n";
-    cout << "2. View inventory\n";
-    cout << "3. Check quantity\n";
-    cout << "4. Exit\n";
-
-     string choice;
-     cout << "Enter choice (1-4)";
-     getline(cin, choice);
-     if (choice == "1" ) {
-        string fruit;
-        int quantity;
-
-        cout << "Fruit name: ";
-        getline(cin, fruit);
-
-        cout << "Quantity: ";
-        cin >> quantity;
-        cin.ignore(); // To consume the newline character
-
-        inventory[fruit] = quantity;
-        cout << quantity << " " << fruit << " added!\n";
-
-     }
-     //this line is true if the input is 2
-     else if (choice == "2") {
-        cout << " While fruit? ";
-        gerline(cin, fruit);
-
-        if (inventory. find(fruit) != inventory. end()) {
-                cout << " There are " << inventory[fruit] <<" " <<fruit <<
-                " available\n";}
-
-        else {
-                cout << "No  << fruit << " in inventory\n";
-
-
-        }
-
-
-        }
-        return 0;
- }
-
+int main() {
+// Simple if statement
+  int age = 25;
+  count << "1. Simple if example:\n";
+  if (age >= 18){
+    cout << "you are an adult.\n"
 
 }
+
+ // if-else statement
+ cout << "\n2. if-else example:\n";
+ if (age < 13) {
+    cout << "you are a child.\n";
+    else  {
+        cout << " you are a teenager or adult.\n";
+
+    }
+
+    // if-else if ladder
+    cout << "\n3. if-else if ladder example:\n";
+    if (age < 13) {
+        cout << "Child\n";
+    } else if (age < 20) {
+        cout << "Teenager\n";
+    } else if (age < 65) {
+        cout << "Adult\n";
+    } else {
+        cout << "Senior\n";
+
+    }
+
+    // Ternary operator
+    cout <<"n4. Ternary operator example:\n";
+    string status = (age >= 18) ? "Can vote" : "Cannot vote";
+    count << " Voting status: " << status << "\n.";
+
+    //Nested if statements
+
+ cout << "\n5. Nested if example:\n";
+    bool hasLicense = true;
+    if (age >= 16) {
+        if (hasLicense) {
+            cout << "You can drive!\n";
+        } else {
+            cout << "You're old enough but need a license.\n";
+        }
+    } else {
+        cout << "You're too young to drive.\n";
+    }
+
+    return 0;
+}}
